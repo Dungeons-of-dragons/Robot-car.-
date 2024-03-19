2acc87aafba6ca601771fe9b9fb2653839a4b891
shawn_phy <61213955+shawn-phy@users.noreply.github.com>
2024-03-19 18:19:34 +0300

    F/ultrasonic distance (#6)
    * Done with ultrasonic distance sensor
    
    * corrected wrong file placement
    
    * Added testing lines
    
    * f(ultrasonic_distance): restructured folder
    
    ---------
    
    Co-authored-by: ANTH0RNY <101851338+ANTH0RNY@users.noreply.github.com>

---
+++ ultrasonic/ultraSonic.py
@@ -0,0 +1,53 @@
+from machine import Pin
+import utime
+
+class UltraSonic:
+    def __init__(self, echo, trig):
+        self.echo = echo
+        self.trig = trig
+    def distance (self):        
+        new_reading = False
+        counter = 0
+        distance = 0
+        duration = 0
+        
+        echoPIN = Pin(self.echo, Pin.IN)
+        trigPIN = Pin(self.trig, Pin.OUT)
+        
+        trigPIN.value(0)
+        utime.sleep_us(2)
+        trigPIN.value(1)
+        utime.sleep_us(10)
+        trigPIN.value(0)
+        utime.sleep_us(2)
+        
+        while not echoPIN.value():
+            # pass
+            counter += 1
+            if counter == 5000:
+                new_reading = True
+                break
+
+        if new_reading:
+            return False
+        
+        startT = utime.ticks_us()/1000000
+        while echoPIN.value(): pass
+        feedbackT = utime.ticks_us()/1000000
+     
+        if feedbackT == startT:
+            distance = "N/A"
+        else:
+            duration = feedbackT - startT
+            soundSpeed = 34300 # cm/s
+            distance = duration * soundSpeed / 2
+            distance = round(distance, 1)
+     
+        return distance
+
+# echo = 0
+# trigger = 1
+# ultra = UltraSonic(echo, trigger)
+# while True:
+#    print (" Distance: " + str(ultra.distance())+ "   ", end='\r')
+#    utime.sleep(0.5)
