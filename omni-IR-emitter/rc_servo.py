c4774c9a00a304a41f8b6861541f8962d832de60
shawn-phy <philipoyuga14@gmail.com>
2024-03-20 20:08:51 +0300

    f(testingintergrations): removed useless comments

--- omni-IR-emitter/rc_servo.py
+++ omni-IR-emitter/rc_servo.py
@@ -2,7 +2,7 @@
 # pulse width values.
 import machine
 import math
-from infrared_phototransistor import is_signal_detected  # Import the signal detection function
+from infrared_phototransistor import is_signal_detected 
 
 class Servo:
     def __init__(self,pin_id,min_us=544.0,max_us=2400.0,min_deg=0.0,max_deg=180.0,freq=50):
