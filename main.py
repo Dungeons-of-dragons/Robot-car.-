8380bd12a779897ab411d416ebffbbdbd22f2ac8
shawn-phy <philipoyuga14@gmail.com>
2024-03-23 18:51:22 +0300

    f(testingintergrations): changed motor driver

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from ultraSonic import UltraSonic
-from MotorDriver import L298N, Motors, my_sleep
+from motorDriver import L298N, Motors, my_sleep
 import utime
 
-LEFT_ENA = 0
-LEFT_IN1 = 2
-LEFT_IN2 = 1
-RIGHT_ENA = 3
-RIGHT_IN1 = 5
-RIGHT_IN2 = 4
+LEFT_ENA = 10
+LEFT_IN1 = 12
+LEFT_IN2 = 11
+RIGHT_ENA = 15
+RIGHT_IN1 = 13
+RIGHT_IN2 = 14
 DEFAULT_SPEED = 65534
 echo = 7
 trigger = 8
 ultra = UltraSonic(echo, trigger)
 motors=Motors()
-dist = 10
+dist = 25
 
 
 def obstacle_detection():
@@ -32,7 +32,7 @@ def obstacle_avoidance():
 
 while True:
     print (" Distance: " + str(ultra.distance())+ "   ", end='\r')
-    motors.manual_control(30000, 30000)
+    motors.start()
     my_sleep(500)
 
 
